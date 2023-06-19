af3dbdc7c04b94bf22d7ac88ead89eea39199302
Onyedikachi123 <onyedikachierugo@gmail.com>
2023-06-19 14:28:49 +0100

    fixed user exists already error

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { getErrorMsg } from "../helpers/index"
+import { getErrorMsg } from "../helpers/index";
 // import { useRouter } from "next/router";
-import axios from 'axios'
+import axios from "axios";
 
 const Signup = () => {
   const [data, setData] = useState({
@@ -14,17 +14,17 @@ const Signup = () => {
     phoneNumber: "",
     ssn: "",
     driverLicense: "",
-    position: ""
-  })
+    position: "",
+  });
 
-  const [validationErrors, setValidationErrors] = useState([])
-  const [submitError, setSubmitError] = useState("")
-  const [loading, setLoading] = useState(false)
+  const [validationErrors, setValidationErrors] = useState([]);
+  const [submitError, setSubmitError] = useState("");
+  const [loading, setLoading] = useState(false);
   const [position, setPosition] = useState("");
   // const router = useRouter()
 
   const validateData = () => {
-    const err = []
+    const err = [];
     if (!data.firstName || data.firstName.length < 4) {
       err.push({ firstName: "First name must be at least 4 characters long" });
     }
@@ -37,7 +37,7 @@ const Signup = () => {
     if (!data.email || !/\S+@\S+\.\S+/.test(data.email)) {
       err.push({ email: "Valid email is required" });
     }
-    if (!data.phoneNumber || !/^\d{10}$/.test(data.phoneNumber)) {
+    if (!data.phoneNumber || !/^\d{11}$/.test(data.phoneNumber)) {
       err.push({ phoneNumber: "Valid phone number is required" });
     }
     if (!data.ssn || !/^\d{9}$/.test(data.ssn)) {
@@ -50,30 +50,44 @@ const Signup = () => {
       err.push({ position: "Position is required" });
     }
     console.log(err);
-    setValidationErrors(err)
+    setValidationErrors(err);
     if (err.length > 0) {
-      return false
+      return false;
+    } else {
+      return true;
     }
-    else {
-      return true
-    }
-  }
+  };
 
   const handleSignup = async (event) => {
     try {
-      event.preventDefault()
-      const isValid = validateData()
+      event.preventDefault();
+      const isValid = validateData();
       console.log(isValid, validationErrors);
       if (isValid) {
-        console.log('enter');
+        console.log("enter");
         try {
-          setLoading(true)
+          setLoading(true);
           console.log({ data });
-          const apiRes = await axios.post("http://localhost:3000/api/auth/signup", data)
+          const apiRes = await axios.post(
+            "http://localhost:3000/api/auth/signup",
+            data
+          );
           console.log(apiRes);
           if (apiRes.data.success) {
             // save data in session using next auth
-            setMessage("Information stored successfully")
+            alert("User has been submitted successfully!");
+            // Clear the form
+            setData({
+              firstName: "",
+              lastName: "",
+              fullAddress: "",
+              email: "",
+              phoneNumber: "",
+              ssn: "",
+              driverLicense: "",
+              position: "",
+            });
+            // setMessage("Information stored successfully")
             //     const loginRes = await loginUser({
             //       email: data.email,
             //       password: data.password
@@ -88,16 +102,16 @@ const Signup = () => {
           }
         } catch (error) {
           if (error && error.response && error.response.data) {
-            const errorMsg = error.response.data.error
-            setSubmitError(errorMsg)
+            const errorMsg = error.response.data.error;
+            setSubmitError(errorMsg);
           }
         }
-        setLoading(false)
+        setLoading(false);
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const handleInputChange = (event) => {
     setData({
@@ -107,9 +121,9 @@ const Signup = () => {
   };
 
   const handleChange = (event) => {
-    setData((prev) => ({ ...prev, position: event.target.value }))
+    setData((prev) => ({ ...prev, position: event.target.value }));
     // setPosition(event.target.value)
-  }
+  };
 
   return (
     <>
@@ -224,13 +238,15 @@ const Signup = () => {
                 Upload your driver lincense or state ID
               </label>
 
-              <input type="file" id="file" className="custom-input"
+              <input
+                type="file"
+                id="file"
+                className="custom-input"
                 name="driverLicense"
                 value={data.driverLicense}
                 onChange={handleInputChange}
                 error={getErrorMsg("driverLicense", validateData)}
               />
-
             </div>
             <div className="form-group">
               <label htmlFor="position" className="form-label">
@@ -239,18 +255,26 @@ const Signup = () => {
                   (if you cant find what you are looking for click on
                   &apos;other&apos;)
                 </p>
-
-                <select name="position" id="position" value={data.position} onChange={handleChange}>
-                  <option value="volvo">Customer service</option>
-                  <option value="saab">Data Entry</option>
-                  <option value="mercedes">Virtual Assistant</option>
-                  <option value="audi">Sales Assistant</option>
-                  <option value="audi">Virtual Book Keeper</option>
-                  <option value="audi">Other</option>
+                <select
+                  name="position"
+                  id="position"
+                  value={data.position}
+                  onChange={handleChange}
+                >
+                  <option value="Customer Service">Customer service</option>
+                  <option value="Data Entry">Data Entry</option>
+                  <option value="Virtual Assistant">Virtual Assistant</option>
+                  <option value="Sales Assistant">Sales Assistant</option>
+                  <option value="Virtual Book Keeper">
+                    Virtual Book Keeper
+                  </option>
+                  <option value="Other">Other</option>
                 </select>
               </label>
             </div>
-            <button className="custom-button errorText" type="submit">SUBMIT</button>
+            <button className="custom-button errorText" type="submit">
+              SUBMIT
+            </button>
           </form>
         </div>
       </div>
